3c681135d659d0484160f2c2e14b5f0d78fd4ea5
LucoMoro <morelli.2000luca@gmail.com>
2025-04-01 10:24:22 +0100

    feat: improve function to move files between folders

--- scraping/googlegit_scraper.py
+++ scraping/googlegit_scraper.py
@@ -2,6 +2,7 @@ import requests
 import os
 from bs4 import BeautifulSoup
 import json
+import shutil
 
 class GoogleGitScraper:
     def __init__(self, url, base_path):
@@ -10,6 +11,23 @@ class GoogleGitScraper:
 
     # Function to fetch and save diffs
     def fetch_and_save_diffs_with_filenames(self):
+        """
+        Fetches diff content and related information from a URL.
+
+        This function sends an HTTP GET request to the URL specified in the instance,
+        parses the HTML response to extract diff tags, unified diff tags, and content
+        from specific HTML elements.
+
+        Returns:
+            tuple: A tuple containing three elements:
+                - list: A list of BeautifulSoup Tag objects representing diff tags.
+                - list: A list of BeautifulSoup Tag objects representing unified diff tags.
+                - str: A string containing the content extracted from the MetadataMessage tag.
+                     Returns an empty string if the tag is not found.
+
+        Raises:
+            requests.exceptions.RequestException: If an error occurs during the HTTP request.
+        """
         try:
             response = requests.get(self.url)
             response.raise_for_status()  # Check for HTTP errors
@@ -29,6 +47,23 @@ class GoogleGitScraper:
 
 
     def save_into_file(self, diff_tags, unified_diff_tags, content, review_id):
+        """
+            Saves diff information into a JSON file.
+
+            This function takes lists of diff tags and unified diff tags, along with content
+            and a review ID, and structures this information into a JSON format. Each diff
+            is associated with its filename and content, and this data is saved into a file
+            named "CR_{review_id}.json" within the "results" subdirectory of the base path.
+
+            Args:
+                diff_tags (list): A list of BeautifulSoup Tag objects representing diff tags.
+                unified_diff_tags (list): A list of BeautifulSoup Tag objects representing unified diff tags.
+                content (str): A string containing the main content or task description.
+                review_id (str): A string representing the review ID, used in the output filename.
+
+            Returns:
+                None
+            """
         cr_files = []
 
         if diff_tags and unified_diff_tags and content:
@@ -51,3 +86,4 @@ class GoogleGitScraper:
 
             with open(output_file, "w") as output:
                 json.dump(data, output, indent=4)
+
--- scraping/main.py
+++ scraping/main.py
@@ -1,26 +1,106 @@
+import json
+import random
+import time
+import os
 from googlegit_scraper import GoogleGitScraper
-from scraping.config import base_path
-from scraping.scraper_manager import ScraperManager
+from config import base_path
+from scraper_manager import ScraperManager
 
-# Hardcoded values
-#review_id = "51750"
-#project = "platform/packages/apps/Settings"
-#git_revision = "ca714d8d0c11c904b25bc20a0a9b2f2cc8d78ad5"
-#project_url = f"{base_url}{project}/+/{git_revision}%5E%21/"
 
-scraping_manager = ScraperManager(base_path)
-ids, projects, patches, revisions = scraping_manager.open_excel()
+class ScraperController:
+    def __init__(self, base_path, max_retries=3):
+        self.base_path = base_path
+        self.max_retries = max_retries
+        self.scraping_manager = ScraperManager(base_path)
+        self.base_url = "https://android.googlesource.com/"
+        self.ids = []
+        self.projects = []
+        self.patches = []
+        self.revisions = []
 
-# URL construction
-base_url = "https://android.googlesource.com/"
 
-# Execute the function
-for i in range(0, 10):
-    current_id = ids[i]
-    current_project = projects[i]
-    current_patch = patches[i]
-    current_revision = revisions[i]
-    project_url = f"{base_url}{current_project}/+/{current_revision}%5E%21/"
-    scraper = GoogleGitScraper(project_url, base_path)
-    diff_tags, unified_diff_tags, content = scraper.fetch_and_save_diffs_with_filenames()
-    scraper.save_into_file(diff_tags, unified_diff_tags, content, current_patch)
+    def load_data(self):
+        """Loads data from the Excel file."""
+        self.ids, self.projects, self.patches, self.revisions = self.scraping_manager.open_excel()
+
+    def scrape(self, start=1, end=3):
+        """Runs the scraping process for a given range."""
+        for i in range(start, end):
+            sleep_time = random.uniform(5, 10)
+            time.sleep(sleep_time)
+
+            current_id = self.ids[i]
+            current_project = self.projects[i]
+            current_patch = self.patches[i]
+            current_revision = self.revisions[i]
+            project_url = f"{self.base_url}{current_project}/+/{current_revision}%5E%21/"
+
+            print(f"Iteration number: {i}")
+            scraper = GoogleGitScraper(project_url, self.base_path)
+            retries = 0
+            success = False
+
+            while retries < self.max_retries and not success:
+                try:
+                    if retries != 0:
+                        time.sleep(30)
+
+                    diff_tags, unified_diff_tags, content = scraper.fetch_and_save_diffs_with_filenames()
+                    scraper.save_into_file(diff_tags, unified_diff_tags, content, current_patch)
+                    success = True  # Mark as successful if no error occurs
+                except Exception as e:
+                    retries += 1
+                    if retries == self.max_retries:
+                        with open('errors.txt', 'a') as error_file:
+                            error_file.write(f"{i}\n")
+                        break  # Move to the next iteration
+
+    def move_files(self):
+        """Moves files from the results folder to pure_java_crs."""
+        source_folder = os.path.join(self.base_path, "results")
+        destination_folder = os.path.join(self.base_path, "pure_java_crs")
+        os.makedirs(destination_folder, exist_ok=True)
+        pure_java = True
+        i = 0
+
+        for file in os.listdir(source_folder):
+            i = i + 1
+            change_request_files = self.filter_files_based_on_language(file)
+
+            # Check if all files in change_request_files are Java files
+            pure_java = all('.java' in change_request_file for change_request_file in change_request_files)
+
+            # Define destination folder based on pure_java check
+            destination_folder = os.path.join(self.base_path, "pure_java_crs" if pure_java else "no_java_crs")
+
+            # Ensure the destination folder exists
+            os.makedirs(destination_folder, exist_ok=True)
+
+            # Move the file
+            file_path = os.path.join(source_folder, file)
+            if os.path.isfile(file_path):
+                os.rename(file_path, os.path.join(destination_folder, file))
+            print(f"Iteration on moving files: {i}")
+
+        print("Files moved successfully.")
+
+    def filter_files_based_on_language(self, file):
+        file_path = os.path.join(self.base_path, f"results/{file}")
+        try:
+            with open(file_path, "r") as change_request:
+                change_request_data = json.load(change_request)
+
+                changed_files = change_request_data.get("files", {})
+                filenames = [changed_file["filename"].split(" ")[2] for changed_file in changed_files]
+
+            return filenames
+        except FileNotFoundError:
+            print(f"Error: File {file_path} not found")
+        except json.JSONDecodeError:
+            print("Error while reading the JSON file")
+
+if __name__ == "__main__":
+    controller = ScraperController(base_path)
+    controller.load_data()
+    controller.scrape()  # Call to start scraping
+    controller.move_files()  # Call to move files
